c314826b7bac64efcc53365ac6c0c39bc4e9433a
loisferrari <98797685+loisferrari@users.noreply.github.com>
2022-02-01 04:00:40 -0300

    Update InfoModal.tsx

--- src/components/modals/InfoModal.tsx
+++ src/components/modals/InfoModal.tsx
@@ -10,41 +10,41 @@ export const InfoModal = ({ isOpen, handleClose }: Props) => {
   return (
     <BaseModal title="How to play" isOpen={isOpen} handleClose={handleClose}>
       <p className="text-sm text-gray-500 dark:text-gray-300">
-        Guess the WORDLE in 6 tries. After each guess, the color of the tiles
+        Guess the GRISHA WORD in 6 tries. After each guess, the color of the tiles
         will change to show how close your guess was to the word.
       </p>
 
       <div className="flex justify-center mb-1 mt-4">
-        <Cell value="W" status="correct" />
-        <Cell value="E" />
+        <Cell value="R" status="correct" />
         <Cell value="A" />
-        <Cell value="R" />
-        <Cell value="Y" />
+        <Cell value="V" />
+        <Cell value="K" />
+        <Cell value="A" />
       </div>
       <p className="text-sm text-gray-500 dark:text-gray-300">
-        The letter W is in the word and in the correct spot.
+        The letter R is in the word and in the correct spot.
+      </p>
+
+      <div className="flex justify-center mb-1 mt-4">
+        <Cell value="S" />
+        <Cell value="A" />
+        <Cell value="N" status="present" />
+        <Cell value="K" />
+        <Cell value="T" />
+      </div>
+      <p className="text-sm text-gray-500 dark:text-gray-300">
+        The letter N is in the word but in the wrong spot.
       </p>
 
       <div className="flex justify-center mb-1 mt-4">
         <Cell value="P" />
-        <Cell value="I" />
-        <Cell value="L" status="present" />
-        <Cell value="O" />
-        <Cell value="T" />
-      </div>
-      <p className="text-sm text-gray-500 dark:text-gray-300">
-        The letter L is in the word but in the wrong spot.
-      </p>
-
-      <div className="flex justify-center mb-1 mt-4">
-        <Cell value="V" />
         <Cell value="A" />
-        <Cell value="G" />
-        <Cell value="U" status="absent" />
-        <Cell value="E" />
+        <Cell value="R" />
+        <Cell value="E" status="absent" />
+        <Cell value="M" />
       </div>
       <p className="text-sm text-gray-500 dark:text-gray-300">
-        The letter U is not in the word in any spot.
+        The letter E is not in the word in any spot.
       </p>
     </BaseModal>
   )
